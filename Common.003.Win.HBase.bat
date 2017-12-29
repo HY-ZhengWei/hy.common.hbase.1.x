fa15cffe9055338174ad6d077dde6008fc0c31cb
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 16:10:40 +0800

    1.6.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.HBase.bat
+++ Common.003.Win.HBase.bat
@@ -4,7 +4,7 @@ cd .\bin
 
 rd /s/q .\org\hy\common\hbase\junit
 
-jar cvfm hy.common.hbase.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.hbase.jar MANIFEST.MF META-INF org
 
 copy hy.common.hbase.jar ..
 del /q hy.common.hbase.jar
--- Common.004.Mac.HBase.sh
+++ Common.004.Mac.HBase.sh
@@ -4,7 +4,7 @@ cd ./bin
 
 rm -R ./org/hy/common/hbase/junit
 
-jar cvfm hy.common.hbase.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.hbase.jar MANIFEST.MF META-INF org
 
 cp hy.common.hbase.jar ..
 rm hy.common.hbase.jar
